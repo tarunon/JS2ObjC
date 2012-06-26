2f18b0cf67c77bb4d768b5090d89d5708994f4df
laiso <laiso@lai.so>
2012-06-26 18:47:15 +0900

    HelloWorls サンプルアプリ追加

---
+++ Examples/HelloWorld/JS2ObjCSample/RootViewController.m
@@ -0,0 +1,31 @@
+#import "RootViewController.h"
+
+#import "JS2ObjC.h"
+
+@implementation RootViewController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        JS2ObjC* js2objc = [JS2ObjC standardJS2ObjC];
+        [js2objc addTarget:self action:@selector(helloWorld) withJSFunctionName:@"helloWorld"];
+    }
+    return self;
+}
+
+- (void)loadView
+{
+    UIWebView* webView = [[UIWebView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    NSURL* url = [[[NSBundle mainBundle] resourceURL] URLByAppendingPathComponent:@"server/index.html"];
+    [webView loadRequest:[NSURLRequest requestWithURL:url]];
+    //webView.delegate = self; // ここを有効にするとWebViewのロードが失敗してる
+    self.view = webView;
+}
+
+- (void)helloWorld
+{
+    NSLog(@"Hello World");
+}
+
+@end
